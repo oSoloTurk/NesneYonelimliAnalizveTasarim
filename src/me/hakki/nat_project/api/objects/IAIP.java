9a773b842408308a5e748333637cce03c4f9c365
Hakkı <hakki.ceylan2356@gmail.com>
2022-03-24 21:49:35 +0300

    added methods to interface of aip

--- src/me/hakki/nat_project/api/objects/IAIP.java
+++ src/me/hakki/nat_project/api/objects/IAIP.java
@@ -1,5 +1,10 @@
 package me.hakki.nat_project.api.objects;
 
+import me.hakki.nat_project.api.databases.IDatabaseHandler;
+
 public interface IAIP {
-    void start();
+    void sogutucuyuAc();
+    void sogutucuyuKapat();
+    float sicaklikOku();
+    IDatabaseHandler getDatabaseHandler();
 }
