af9cec6d552f3f39aae806845f0f32cddaaec072
Hakkı <hakki.ceylan2356@gmail.com>
2022-03-24 21:49:19 +0300

    implementation of mongodb handler

--- src/me/hakki/nat_project/databases/MongoHandler.java
+++ src/me/hakki/nat_project/databases/MongoHandler.java
@@ -9,7 +9,10 @@ import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import me.hakki.nat_project.api.databases.IDatabaseHandler;
 import me.hakki.nat_project.objects.Kullanici;
+import me.hakki.nat_project.utils.Generator;
 import org.bson.Document;
+
+import java.time.LocalDateTime;
 
 public class MongoHandler implements IDatabaseHandler {
     private MongoClient client;
@@ -32,5 +35,30 @@ public class MongoHandler implements IDatabaseHandler {
         if(!response.iterator().hasNext()) return null;
         Document userDocument = response.iterator().next();
         return new Kullanici(userDocument.getString("username"), null);
+    }
+
+    @Override
+    public float sicaklikOku() {
+        if(client == null) return 0f;
+        BasicDBObject sort_filter = new BasicDBObject();
+        sort_filter.put("time", 1);
+        FindIterable<Document> response = client.getDatabase("nat_project")
+                .getCollection("temperatures")
+                .find()
+                .sort(sort_filter);
+        if(!response.iterator().hasNext()) return Generator.getRandomFloat(20, 50);
+        Document userDocument = response.iterator().next();
+        return userDocument.getDouble("temperature").floatValue();
+    }
+
+    @Override
+    public void sicaklikYaz(float yeniDeger) {
+        if(client == null) return;
+        Document document = new Document();
+        document.put("time", LocalDateTime.now());
+        document.put("temperature", LocalDateTime.now());
+        client.getDatabase("nat_project")
+                .getCollection("temperatures")
+                .insertOne(document);
     }
 }
