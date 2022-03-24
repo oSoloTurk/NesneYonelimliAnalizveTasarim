ad551e33a3a455e4d85d5d26101e17d2122568c3
Hakkı <hakki.ceylan2356@gmail.com>
2022-03-24 21:49:49 +0300

    changed return type of a method

--- src/me/hakki/nat_project/api/objects/IAgArayuzu.java
+++ src/me/hakki/nat_project/api/objects/IAgArayuzu.java
@@ -3,5 +3,5 @@ package me.hakki.nat_project.api.objects;
 public interface IAgArayuzu {
     void sogutucuyuAc();
     void sogutucuyuKapat();
-    void sicaklikGonder();
+    int sicaklikGonder();
 }
