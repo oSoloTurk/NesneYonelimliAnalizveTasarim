f59c959b0351bbcd807adc65c99b54b17b05bd84
Hakkı <hakki.ceylan2356@gmail.com>
2022-03-24 21:50:49 +0300

    added some methods to db handler

--- src/me/hakki/nat_project/api/databases/IDatabaseHandler.java
+++ src/me/hakki/nat_project/api/databases/IDatabaseHandler.java
@@ -5,4 +5,6 @@ import me.hakki.nat_project.objects.Kullanici;
 public interface IDatabaseHandler {
 
     Kullanici girisYap(final String kullaniciAdi, final String sifre);
+    float sicaklikOku();
+    void sicaklikYaz(final float yeniDeger);
 }
