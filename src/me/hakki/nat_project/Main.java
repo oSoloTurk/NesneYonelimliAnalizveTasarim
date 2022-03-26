126ca5bf1c97899d8f6caf1a056c1f8424f18b65
Hakkı <hakki.ceylan2356@gmail.com>
2022-03-26 20:26:37 +0300

    added initializer for the universal temperature engine

--- src/me/hakki/nat_project/Main.java
+++ src/me/hakki/nat_project/Main.java
@@ -1,12 +1,15 @@
 package me.hakki.nat_project;
 
+import me.hakki.nat_project.api.objects.IAIP;
 import me.hakki.nat_project.objects.AIP;
+import me.hakki.nat_project.objects.sicaklik_motoru.SicaklikMotoru;
 
 public class Main {
 
     public static void main(String[] args) {
-        //TODO: load enviroments in here
-        AIP aip = new AIP();
-        aip.start();
+        //TODO: load environments in here
+        IAIP aip = new AIP();
+
+        SicaklikMotoru sicaklikMotoru = SicaklikMotoru.getInstance(); //create a instance of universal sicaklikMotoru
     }
 }
