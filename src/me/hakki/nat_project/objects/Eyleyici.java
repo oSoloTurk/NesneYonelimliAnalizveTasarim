1b1957c35bdaf9d0a7eb12940304cb69c07033b2
Hakkı <hakki.ceylan2356@gmail.com>
2022-03-26 20:24:22 +0300

    sogutucu actions splitted to classes

--- src/me/hakki/nat_project/objects/Eyleyici.java
+++ src/me/hakki/nat_project/objects/Eyleyici.java
@@ -1,50 +1,21 @@
 package me.hakki.nat_project.objects;
 
-import me.hakki.nat_project.api.databases.IDatabaseHandler;
 import me.hakki.nat_project.api.objects.IAIP;
 import me.hakki.nat_project.api.objects.IEyleyici;
-
-import java.security.SecureRandom;
+import me.hakki.nat_project.objects.sicaklik_motoru.SicaklikMotoru;
 
 public class Eyleyici implements IEyleyici {
 
-    protected IAIP aip;
-    protected SecureRandom random;
-    private SogutucuAcma sogutucuAcma;
-    private SogutucuKapatma sogutucuKapatma;
-
-    public Eyleyici(IAIP aip){
-        this.aip = aip;
-        this.random = new SecureRandom();
-        this.sogutucuAcma = new SogutucuAcma();
-        this.sogutucuKapatma = new SogutucuKapatma();
-        //TODO: kapatma active
+    public Eyleyici(){
     }
 
     @Override
     public void sogutucuyuAc() {
-        //TODO: acma active kapatma close
+        SicaklikMotoru.getInstance().sogutucuyuAc();
     }
 
     @Override
     public void sogutucuyuKapat() {
-        //TODO: acma close kapatma active
-    }
-
-    public class SogutucuAcma extends Thread {
-        @Override
-        public void run(){
-            float volume = random.nextFloat();
-            aip.getDatabaseHandler().sicaklikYaz(aip.sicaklikOku() - volume);
-        }
-    }
-
-    public class SogutucuKapatma extends Thread {
-        private final int ODA_SICAKLIGI = 25;
-        @Override
-        public void run(){
-            float volume = random.nextFloat();
-            aip.getDatabaseHandler().sicaklikYaz((ODA_SICAKLIGI - aip.sicaklikOku()) * volume);
-        }
+        SicaklikMotoru.getInstance().sogutucuyuKapat();
     }
 }
