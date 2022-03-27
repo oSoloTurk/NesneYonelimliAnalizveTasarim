a76e6a2a79303b84b6e1232bb3d6fbc2fe98d4cc
Hakkı <hakki.ceylan2356@gmail.com>
2022-03-27 23:48:12 +0300

    implemented dotenv loader

--- src/me/hakki/nat_project/Main.java
+++ src/me/hakki/nat_project/Main.java
@@ -2,17 +2,19 @@ package me.hakki.nat_project;
 
 import me.hakki.nat_project.api.objects.IAIP;
 import me.hakki.nat_project.api.objects.kullanici_client.IKullaniciClient;
+import me.hakki.nat_project.api.objects.sicaklik_motoru.ISicaklikMotoru;
 import me.hakki.nat_project.objects.AIP;
 import me.hakki.nat_project.objects.kullanici_client.KullaniciClient;
 import me.hakki.nat_project.objects.sicaklik_motoru.SicaklikMotoru;
+import me.hakki.nat_project.utils.DotEnv;
 
 public class Main {
 
     public static void main(String[] args) {
-        //TODO: load environments in here
+        DotEnv.load();
         IAIP aip = new AIP();
 
-        SicaklikMotoru sicaklikMotoru = SicaklikMotoru.getInstance(); //create a instance of universal sicaklikMotoru
+        ISicaklikMotoru sicaklikMotoru = SicaklikMotoru.getInstance(); //create a instance of universal sicaklikMotoru
 
         IKullaniciClient client = new KullaniciClient();
         client.baglan(aip.getAgArayuzu());
