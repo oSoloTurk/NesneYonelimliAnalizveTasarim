ea46dd88f7392942ceacf9f7a3f858060c1affc9
Hakkı <hakki.ceylan2356@gmail.com>
2022-03-27 10:50:52 +0300

    implemented network interface

--- src/me/hakki/nat_project/api/objects/IAgArayuzu.java
+++ src/me/hakki/nat_project/api/objects/IAgArayuzu.java
@@ -3,5 +3,5 @@ package me.hakki.nat_project.api.objects;
 public interface IAgArayuzu {
     void sogutucuyuAc();
     void sogutucuyuKapat();
-    int sicaklikGonder();
+    float sicaklikGonder();
 }
--- src/me/hakki/nat_project/objects/AgArayuzu.java
+++ src/me/hakki/nat_project/objects/AgArayuzu.java
@@ -1,20 +1,27 @@
 package me.hakki.nat_project.objects;
 
+import me.hakki.nat_project.api.objects.IAIP;
 import me.hakki.nat_project.api.objects.IAgArayuzu;
 
 public class AgArayuzu implements IAgArayuzu {
+    private final IAIP aip;
+
+    public AgArayuzu(IAIP aip) {
+        this.aip = aip;
+    }
+
     @Override
     public void sogutucuyuAc() {
-
+        aip.sogutucuyuAc();
     }
 
     @Override
     public void sogutucuyuKapat() {
-
+        aip.sogutucuyuKapat();
     }
 
     @Override
-    public int sicaklikGonder() {
-        return 0;
+    public float sicaklikGonder() {
+        return aip.sicaklikOku();
     }
 }
