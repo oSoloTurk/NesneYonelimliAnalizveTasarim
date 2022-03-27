e0187fea4dcb6498acced5d0ca735ac5466ca7ae
Hakkı <hakki.ceylan2356@gmail.com>
2022-03-27 23:47:58 +0300

    created environment loader

---
+++ src/me/hakki/nat_project/utils/DotEnv.java
@@ -0,0 +1,29 @@
+package me.hakki.nat_project.utils;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class DotEnv {
+    public static void load() {
+        File file = new File(".env");
+        if (!file.exists()) {
+            System.out.println(".env file not exists!");
+            return;
+        }
+        Scanner scanner = null;
+        try {
+            scanner = new Scanner(file);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        int counter = 0;
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            int seperatorIndex = line.indexOf("=");
+            System.getProperties().setProperty(line.substring(0, seperatorIndex), line.substring(seperatorIndex + 1));
+            counter++;
+        }
+        System.out.println("Loaded " + counter + " environment from .env");
+    }
+}
