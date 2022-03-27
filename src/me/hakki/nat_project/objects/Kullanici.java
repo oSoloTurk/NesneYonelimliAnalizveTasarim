79566839db21a83ae1f62a792bc3a645f313133a
Hakkı <hakki.ceylan2356@gmail.com>
2022-03-27 23:49:37 +0300

    added permission level for the user

--- src/me/hakki/nat_project/objects/Kullanici.java
+++ src/me/hakki/nat_project/objects/Kullanici.java
@@ -2,14 +2,18 @@ package me.hakki.nat_project.objects;
 
 public class Kullanici {
     private final String kullaniciAdi;
-    private final String sifreHashi;
+    private final int yetkiSeviyesi;
 
-    public Kullanici(String kullaniciAdi, String sifreHashi) {
+    public Kullanici(String kullaniciAdi, int yetkiSeviyesi) {
         this.kullaniciAdi = kullaniciAdi;
-        this.sifreHashi = sifreHashi;
+        this.yetkiSeviyesi = yetkiSeviyesi;
     }
 
     public String getKullaniciAdi() {
         return kullaniciAdi;
+    }
+
+    public int getYetkiSeviyesi() {
+        return yetkiSeviyesi;
     }
 }
