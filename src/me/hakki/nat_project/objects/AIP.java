29cae73220ac41d610d48cf0f5b145e427cf57e6
Hakkı <hakki.ceylan2356@gmail.com>
2022-04-23 23:06:42 +0300

    removed database access of aip and implemented new statuses

--- src/me/hakki/nat_project/objects/AIP.java
+++ src/me/hakki/nat_project/objects/AIP.java
@@ -2,29 +2,34 @@ package me.hakki.nat_project.objects;
 
 import me.hakki.nat_project.api.databases.IDatabaseHandler;
 import me.hakki.nat_project.api.objects.*;
+import me.hakki.nat_project.enums.CihazDurumu;
 import me.hakki.nat_project.factories.ComponentFactory;
 
 public class AIP implements IAIP {
     private final IAgArayuzu agArayuzu;
     private final IEyleyici eyleyici;
-    private final IDatabaseHandler databaseHandler;
     private final ISicaklikAlgilayici sicaklikAlgilayici;
+    private CihazDurumu durum = CihazDurumu.KAPALI;
 
     public AIP(){
         agArayuzu = ComponentFactory.getInstance().getAgArayuzu(this);
         eyleyici = ComponentFactory.getInstance().getEyleyici();
-        databaseHandler = ComponentFactory.getInstance().getDatabaseHandler(DatabaseType.MONGODB);
         sicaklikAlgilayici = ComponentFactory.getInstance().getSicaklikAlgilayici();
+        setDurum(CihazDurumu.BEKLEME);
     }
 
     @Override
     public void sogutucuyuAc() {
+        setDurum(CihazDurumu.ISLEM_YAPIYOR);
         eyleyici.sogutucuyuAc();
+        setDurum(CihazDurumu.BEKLEME);
     }
 
     @Override
     public void sogutucuyuKapat() {
+        setDurum(CihazDurumu.ISLEM_YAPIYOR);
         eyleyici.sogutucuyuKapat();
+        setDurum(CihazDurumu.BEKLEME);
     }
 
     @Override
@@ -32,7 +37,17 @@ public class AIP implements IAIP {
         return sicaklikAlgilayici.sicaklikOku();
     }
 
+    @Override
     public IAgArayuzu getAgArayuzu() {
         return agArayuzu;
+    }
+
+    @Override
+    public String getDurumMesaji(){
+        return durum.getMesaj();
+    }
+
+    private void setDurum(CihazDurumu yeniDurum) {
+        this.durum = yeniDurum;
     }
 }
